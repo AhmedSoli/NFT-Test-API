f37e6a7da7144e45b191927d8a544cb870504f3a
Ahmed Soliman <soliman@uber.com>
2021-08-11 12:00:03 +0200

    Add Metadata API

--- main.py
+++ main.py
@@ -2,6 +2,150 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get("/")
-def hello():
-    return {"message":"Hello TutLinks.com"}
+metadata = {
+	1: {
+		"attributes": [
+			{
+			  "trait_type": "Base", 
+			  "value": "narwhal"
+			}, 
+			{
+			  "trait_type": "Eyes", 
+			  "value": "sleepy"
+			}, 
+			{
+			  "trait_type": "Mouth", 
+			  "value": "cute"
+			}, 
+			{
+			  "trait_type": "Level", 
+			  "value": 4
+			}, 
+			{
+			  "trait_type": "Stamina", 
+			  "value": 90.2
+			}, 
+			{
+			  "trait_type": "Personality", 
+			  "value": "Boring"
+			}, 
+			{
+			  "display_type": "boost_number", 
+			  "trait_type": "Aqua Power", 
+			  "value": 10
+			}, 
+			{
+			  "display_type": "boost_percentage", 
+			  "trait_type": "Stamina Increase", 
+			  "value": 5
+			}, 
+			{
+			  "display_type": "number", 
+			  "trait_type": "Generation", 
+			  "value": 1
+			}
+	  ], 
+	  "description": "Friendly OpenSea Creature that enjoys long swims in the ocean.", 
+	  "external_url": "https://openseacreatures.io/3", 
+	  "image": "https://storage.googleapis.com/opensea-prod.appspot.com/creature/3.png", 
+	  "name": "Dave Starbelly"
+	},
+	2: {
+		"attributes": [
+			{
+			  "trait_type": "Base", 
+			  "value": "narwhal"
+			}, 
+			{
+			  "trait_type": "Eyes", 
+			  "value": "sleepy"
+			}, 
+			{
+			  "trait_type": "Mouth", 
+			  "value": "cute"
+			}, 
+			{
+			  "trait_type": "Level", 
+			  "value": 4
+			}, 
+			{
+			  "trait_type": "Stamina", 
+			  "value": 90.2
+			}, 
+			{
+			  "trait_type": "Personality", 
+			  "value": "Boring"
+			}, 
+			{
+			  "display_type": "boost_number", 
+			  "trait_type": "Aqua Power", 
+			  "value": 10
+			}, 
+			{
+			  "display_type": "boost_percentage", 
+			  "trait_type": "Stamina Increase", 
+			  "value": 5
+			}, 
+			{
+			  "display_type": "number", 
+			  "trait_type": "Generation", 
+			  "value": 1
+			}
+	  ], 
+	  "description": "Friendly OpenSea Creature that enjoys long swims in the ocean.", 
+	  "external_url": "https://openseacreatures.io/3", 
+	  "image": "https://storage.googleapis.com/opensea-prod.appspot.com/creature/3.png", 
+	  "name": "Dave Starbelly"
+	},
+	3: {
+		"attributes": [
+			{
+			  "trait_type": "Base", 
+			  "value": "narwhal"
+			}, 
+			{
+			  "trait_type": "Eyes", 
+			  "value": "sleepy"
+			}, 
+			{
+			  "trait_type": "Mouth", 
+			  "value": "cute"
+			}, 
+			{
+			  "trait_type": "Level", 
+			  "value": 4
+			}, 
+			{
+			  "trait_type": "Stamina", 
+			  "value": 90.2
+			}, 
+			{
+			  "trait_type": "Personality", 
+			  "value": "Boring"
+			}, 
+			{
+			  "display_type": "boost_number", 
+			  "trait_type": "Aqua Power", 
+			  "value": 10
+			}, 
+			{
+			  "display_type": "boost_percentage", 
+			  "trait_type": "Stamina Increase", 
+			  "value": 5
+			}, 
+			{
+			  "display_type": "number", 
+			  "trait_type": "Generation", 
+			  "value": 1
+			}
+	  ], 
+	  "description": "Friendly OpenSea Creature that enjoys long swims in the ocean.", 
+	  "external_url": "https://openseacreatures.io/3", 
+	  "image": "https://storage.googleapis.com/opensea-prod.appspot.com/creature/3.png", 
+	  "name": "Dave Starbelly"
+	}	
+}
+
+@app.get("/tokens/{tokenId}")
+def hello(tokenId : int):
+	return metadata[tokenId]
